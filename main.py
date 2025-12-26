7fbc6726ea0147318a021738f464a9ba6fbbf8ac
R4ben <r4benbrito@gmail.com>
2025-12-26 16:55:50 -0100

    Fazendo a nave navegar

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from player import Player
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        # player.update(dt)
+        # player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
